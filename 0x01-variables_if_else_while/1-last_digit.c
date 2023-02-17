c3640b25c8d51500f274fa1ca1264075140a5ca0
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-02-17 09:11:49 -0800

    Adding 1

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n>5)
+	{
+		print("%d is greater than 5,n")
+	}
+	else if (n==0)
+	{
+		print("%d is equal to zero",n)
+	}
+	else
+	{
+		print("%d is less than six and not zero")
+	}
+	return (0);
+}
