72cbc500782c5b8ee75f43ab92f75dfc3122d202
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-02-21 12:19:33 -0800

    correcting batch

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,35 +1,34 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_to_98 - Check Holberton
- * @n: A input integer
- * Description: function that prints all natural numbers
- * from n to 98, followed by a new line.
- * Return: Nothing
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: int type number
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n > 98)
 	{
-		for (; n <= 98; n++)
+		while (n >= 98)
 		{
 			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
 		}
-		printf("\n");
 	}
 	else
 	{
-		for (; n >= 98; n--)
+		while (n <= 98)
 		{
 			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
-		printf("\n");
 	}
+	printf("\n");
 }
--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * _islower - function to check for lowercase character
- * @c is a variable to check
- * Return: 0
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @c: char type letter
+ * Return: 1 if lowercase, 0 if not lowercase
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
+	if (c > 96 && c < 123)
 		return (1);
-	}
 	else
 		return (0);
 }
