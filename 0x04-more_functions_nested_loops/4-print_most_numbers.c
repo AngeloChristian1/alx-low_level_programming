7d3b4e293dae5ac8fdda023e53848f8066317722
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-02-23 08:12:17 -0800

    adding more

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_most_numbers(void) will print numbers from 1 to 9 except 2 and 4
+ *@i: The integer to check
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	for (int i=0; i < 10; i++)
+	{
+		if (i == 4 || i == 2)
+		{
+		       continue;	
+		}
+	return (0);
+	}
+}
