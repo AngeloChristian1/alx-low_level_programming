c94f5eacf843c3d7b76649925d199a57c8095ce0
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-03-06 09:35:23 -0800

    correcting errors

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
- * _memset - fills memory with a constant byte
- * @s: pointer to memory area to fill
- * @b: constant byte to fill memory with
- * @n: number of bytes to fill
- *
- * Return: pointer to the memory area s
+ * _memcpy - copy memory area
+ * @dest: char array to copy into
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to `dest`
  */
-char *_memset(char *s, char b, unsigned int n)
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	char *yolo;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	yolo = dest;
+	while (n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
 
-	return (s);
+	return (yolo);
 }
-
--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,21 @@
 #include "main.h"
 /**
- * _strchr - locates a character in a string
- * @s: pointer to string to search
- * @c: character to locate
- *
- * Return: pointer to the first occurrence of c in s, or NULL if not found
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to look for
+ * Return: NULL if char not found, or pointer to first occurrence of char `c`
  */
+
 char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
 		s++;
 	}
 
-	if (*s == c)
-		return (s);
-
-	return (NULL);
+	return (s + 1);
 }
-
-
