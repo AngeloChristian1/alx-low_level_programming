cf545215fd2f41782bb8dec024b58b9d6b051ec4
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-03-10 13:19:08 -0800

    correcting

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c *.c
-ar rc liball.a *.o
+
+# This script creates a static library called liball.a from all .c files
+# in the current directory using the functions specified in the header file main.h
+
+# Remove any existing object files and library
+rm -f *.o
+rm -f liball.a
+
+# Compile each .c file and create corresponding object file
+for file in *.c
+do
+    gcc -Wall -Werror -Wextra -pedantic -c "$file" -o "${file%.c}.o"
+done
+
+# Create the static library from all object files
+ar -rc liball.a *.o
+
+# Index the library
+ranlib liball.a
+
+# Clean up object files
+rm -f *.o
+
