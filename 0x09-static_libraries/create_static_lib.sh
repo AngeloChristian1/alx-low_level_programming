78a9288c66d52ee1fc076d2141a788347ab2f652
Gatete Angelo Christian <ishgatetechristian@gmail.com>
2023-03-10 13:24:52 -0800

    try

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,24 +1,3 @@
 #!/bin/bash
-
-# This script creates a static library called liball.a from all .c files
-# in the current directory using the functions specified in the header file main.h
-
-# Remove any existing object files and library
-rm -f *.o
-rm -f liball.a
-
-# Compile each .c file and create corresponding object file
-for file in *.c
-do
-    gcc -Wall -Werror -Wextra -pedantic -c "$file" -o "${file%.c}.o"
-done
-
-# Create the static library from all object files
-ar -rc liball.a *.o
-
-# Index the library
-ranlib liball.a
-
-# Clean up object files
-rm -f *.o
-
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar rc libmy.a *.o
